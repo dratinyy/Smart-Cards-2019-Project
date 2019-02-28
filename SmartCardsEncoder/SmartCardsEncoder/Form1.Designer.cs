eb2e4a268aa75c0bf92b9f3868d791acd65d7ba5
Elliot Kauffmann <elliot.kauffmann@etu.univ-cotedazur.fr>
2019-02-28 21:48:41 +0100

    Encoder can now actually write after encoding.

--- SmartCardsEncoder/SmartCardsEncoder/Form1.Designer.cs
+++ SmartCardsEncoder/SmartCardsEncoder/Form1.Designer.cs
@@ -110,7 +110,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(149, 23);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Encode";
+            this.button2.Text = "Encode and Upload";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
